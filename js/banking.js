dbdac157680759e1b57ad2e845f1b53ccebaa44d
mollikacomputer <mollikacomputer3@gmail.com>
2022-02-11 18:07:39 +0600

    withdraw script added withouttotal  balance

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,27 @@
-// document.getElementById('deposit-button').addEventListener('click', function(){
-//     // console.log('button clicked');
-//     const depositInputBox = document.getElementById('deposit-input-box');
-//     const totalDeposit = document.getElementById('total-deposit');
-//     console.log(depositInputBox);
+// step 1. handle deposit button event
+      
+document.getElementById('deposit-button').addEventListener('click', function(){
+    // step 2: catch input value + new deposit amount into input field
+    const depositInputBox = document.getElementById('deposit-input');
+    let newDepositAmount = depositInputBox.value;
+    // step 3: catch deposit before total amount of money 
+    let beforeDepositTotal = document.getElementById('before-deposit-total');
+    let beforeDepositTotalText= beforeDepositTotal.innerText;
+    let beforeDepositTotalAmount = parseFloat(beforeDepositTotalText);
+    //step 4: existing total + current total 
+    let updateTotalBalanceAmount = parseFloat(newDepositAmount) + parseFloat(beforeDepositTotalText);
+    // step 5: replace of before deposit total money
+     beforeDepositTotal.innerText = updateTotalBalanceAmount;
+     // step 6: Total balance update
+    const previousBalanceText =  document.getElementById('previous-balance-text');
+    const previousBalanceAmount = parseFloat(previousBalanceText.innerText);
+    
+    const updateBalanceTotal = parseFloat(newDepositAmount) + previousBalanceAmount;
 
-//     // const inputAmount = depositInputBox.value;
-//     // const currentAmount = totalDeposit.innerText;
-//     // const updateTotalAmount = inputAmount + currentAmount;
+    previousBalanceText.innerText = updateBalanceTotal;
+    // step 7: withdraw
+    
+    depositInputBox.value = ' ';
+    //  console.log(updateAccountBalance);
+})
 
-// })
